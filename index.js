c6a45771afe498bd5a54ea8ed0987642a841640e
Pepe Becker <mail@pepebecker.com>
2018-02-18 15:36:40 +0100

    Use mdbg instead of find-hanzi

--- index.js
+++ index.js
@@ -1,100 +1,66 @@
 'use strict'
 
+const mdbg = require('mdbg')
 const pinyinOrHanzi = require('pinyin-or-hanzi')
 const convert = require('pinyin-convert')
-const findHanzi = require('find-hanzi')
 const split = require('pinyin-split')
-const so = require('so')
 
-const createDescription = (data) => {
-	let content = ''
-	for (let item of data) {
-		if (item.hanzi) content += `Character: ${item.hanzi}\n`
-		if (item.pinyin) content += `Pinyin: ${item.pinyin}\n`
-		if (item.cangjie) content += `Cangjie: ${item.cangjie} : ${item.cangjie2}\n`
-		if (item.strokes) content += `Strokes: ${item.strokes}\n`
-		if (item.definition) content += `Definition: ${item.definition}\n`
-		content += '\n'
+const getPinyin = async text => {
+	const type = await pinyinOrHanzi(text)
+	if (type !== 'other') {
+		return convert(text, {keepSpaces: true})
+	} else {
+		return text
 	}
-	return content
 }
 
-const getHanzi = (text, callback) => {
-	pinyinOrHanzi(text).then((type) => {
-		if (type == 1) {
-			so(function*() {
-				let response = ''
-				const characters = text.split('')
-				for (let char of characters) {
-					try {
-						const data = yield findHanzi(char)
-						response += createDescription(data)
-					} catch (err) {
-						callback(err)
-					}
-				}
-				if (response.replace(/\n\ /g, '') != '') {
-					callback(response)
-				}
-			})()
+const getSplitted = async text => {
+	const type = await pinyinOrHanzi(text)
+	if (type !== 'mandarin') {
+		const data = await split(text)
+		return data.join(' ')
+	} else {
+		return text
+	}
+}
+
+const getDefinition = async text => {
+	const char = await mdbg.get(text)
+	let content = [char.simplified + '|' + char.traditional]
+	content = content.concat(Object.keys(char.data).map(pinyin => {
+		let defs = char.data[pinyin].definitions
+		if (defs.join(', ').length < 80) {
+			defs = defs.join(', ')
 		} else {
-			findHanzi(text).then((data) => {
-				const response = createDescription(data)
-
-				if (response.replace(/\n\ /g, '') != '') {
-					callback(response)
-				}
-			}).catch(callback)
+			defs = defs.join(',\n    ')
 		}
-	}).catch(callback)
+		return char.data[pinyin].mandarin + ': ' + defs
+	}))
+	return content.join('\n')
 }
 
-const getPinyin = (text, callback) => {
-	pinyinOrHanzi(text).then((type) => {
-		if (type > 0) {
-			convert(text, {keepSpaces: true})
-			.then(callback)
-			.catch(callback)
-		}
-	}).catch(callback)
-}
-
-const getSplitted = (text, callback) => {
-	pinyinOrHanzi(text).then((type) => {
-		if (type !== 1) {
-			split(text).then((data) => {
-				callback(data.join(' '))
-			}).catch(callback)
-		}
-	}).catch(callback)
-}
-
-const processMessage = (text, callback) => new Promise((yay, nay) => {
+const processMessage = async text => {
 	if (/^\/(s|split) /.test(text))
 	{
-		console.log('Received command:', text)
 		text = text.replace('/split ', '')
 		text = text.replace('/s ', '')
-		getSplitted(text, yay)
+		return getSplitted(text)
 	}
 	else if (/^\/(p|pinyin) /.test(text))
 	{
-		console.log('Received command:', text)
 		text = text.replace('/pinyin ', '')
 		text = text.replace('/p ', '')
-		getPinyin(text, yay)
+		return getPinyin(text)
 	}
-	else if (/^\/(h|hanzi) /.test(text))
+	else if (/^\/(d|definition) /.test(text))
 	{
-		console.log('Received command:', text)
-		text = text.replace('/hanzi ', '')
-		text = text.replace('/h ', '')
-		getHanzi(text, yay)
+		text = text.replace('/definition ', '')
+		text = text.replace('/d ', '')
+		return getDefinition(text)
 	}
 	else {
-		console.log('Received message:', text)
-		getPinyin(text, yay)
+		return getPinyin(text)
 	}
-})
+}
 
-module.exports = {processMessage}
+module.exports = { processMessage }
--- test/index.js
+++ test/index.js
@@ -4,71 +4,71 @@ const botCore = require('../index')
 
 describe('Default', () => {
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('我的猫喜欢喝牛奶').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+		return botCore.processMessage('我的猫喜欢喝牛奶').then(data => {
+			data.should.equal('wǒ de māo xǐ huan hē niú nǎi')
 		})
 	})
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('wǒ de māo xǐ huān hē niú nǎi').then((data) => {
-			data.should.equal('wo3 de mao1 xi3 huan1 he1 niu2 nai3')
+		return botCore.processMessage('wǒ de māo xǐ huan hē niú nǎi').then(data => {
+			data.should.equal('wo3 de5 mao1 xi3 huan5 he1 niu2 nai3')
 		})
 	})
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('wo3 de mao1 xi3 huan1 he1 niu2 nai3').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+		return botCore.processMessage('wo3 de5 mao1 xi3 huan5 he1 niu2 nai3').then(data => {
+			data.should.equal('wǒ de māo xǐ huan hē niú nǎi')
 		})
 	})
 })
 
 describe('Pinyin (/p)', () => {
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('/p 我的猫喜欢喝牛奶').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+		return botCore.processMessage('/p 我的猫喜欢喝牛奶').then(data => {
+			data.should.equal('wǒ de māo xǐ huan hē niú nǎi')
 		})
 	})
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('/p wǒ de māo xǐ huān hē niú nǎi').then((data) => {
-			data.should.equal('wo3 de mao1 xi3 huan1 he1 niu2 nai3')
+		return botCore.processMessage('/p wǒ de māo xǐ huan hē niú nǎi').then(data => {
+			data.should.equal('wo3 de5 mao1 xi3 huan5 he1 niu2 nai3')
 		})
 	})
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('/p wo3 de mao1 xi3 huan1 he1 niu2 nai3').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+		return botCore.processMessage('/p wo3 de5 mao1 xi3 huan5 he1 niu2 nai3').then(data => {
+			data.should.equal('wǒ de māo xǐ huan hē niú nǎi')
 		})
 	})
 })
 
 describe('Pinyin (/pinyin)', () => {
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('/pinyin 我的猫喜欢喝牛奶').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+		return botCore.processMessage('/pinyin 我的猫喜欢喝牛奶').then(data => {
+			data.should.equal('wǒ de māo xǐ huan hē niú nǎi')
 		})
 	})
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('/pinyin wǒ de māo xǐ huān hē niú nǎi').then((data) => {
-			data.should.equal('wo3 de mao1 xi3 huan1 he1 niu2 nai3')
+		return botCore.processMessage('/pinyin wǒ de māo xǐ huan hē niú nǎi').then(data => {
+			data.should.equal('wo3 de5 mao1 xi3 huan5 he1 niu2 nai3')
 		})
 	})
 	it('should convert characters to Pinyin', () => {
-		return botCore.processMessage('/pinyin wo3 de mao1 xi3 huan1 he1 niu2 nai3').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+		return botCore.processMessage('/pinyin wo3 de5 mao1 xi3 huan5 he1 niu2 nai3').then(data => {
+			data.should.equal('wǒ de māo xǐ huan hē niú nǎi')
 		})
 	})
 })
 
 describe('Split (/s)', () => {
 	it('should split the text into the correct words', () => {
-		return botCore.processMessage('/s wodemaoxihuanheniunai').then((data) => {
+		return botCore.processMessage('/s wodemaoxihuanheniunai').then(data => {
 			data.should.equal('wo de mao xi huan he niu nai')
 		})
 	})
 	it('should split the text into the correct words', () => {
-		return botCore.processMessage('/s wǒdemāoxǐhuānhēniúnǎi').then((data) => {
+		return botCore.processMessage('/s wǒdemāoxǐhuānhēniúnǎi').then(data => {
 			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
 		})
 	})
 	it('should split the text into the correct words', () => {
-		return botCore.processMessage('/s wo3demao1xi3huan1he1niu2nai3').then((data) => {
+		return botCore.processMessage('/s wo3demao1xi3huan1he1niu2nai3').then(data => {
 			data.should.equal('wo3 de mao1 xi3 huan1 he1 niu2 nai3')
 		})
 	})
@@ -76,31 +76,31 @@ describe('Split (/s)', () => {
 
 describe('Split (/split)', () => {
 	it('should split the text into the correct words', () => {
-		return botCore.processMessage('/split wodemaoxihuanheniunai').then((data) => {
+		return botCore.processMessage('/split wodemaoxihuanheniunai').then(data => {
 			data.should.equal('wo de mao xi huan he niu nai')
 		})
 	})
 	it('should split the text into the correct words', () => {
-		return botCore.processMessage('/split wǒdemāoxǐhuānhēniúnǎi').then((data) => {
+		return botCore.processMessage('/split wǒdemāoxǐhuānhēniúnǎi').then(data => {
 			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
 		})
 	})
 	it('should split the text into the correct words', () => {
-		return botCore.processMessage('/split wo3demao1xi3huan1he1niu2nai3').then((data) => {
+		return botCore.processMessage('/split wo3demao1xi3huan1he1niu2nai3').then(data => {
 			data.should.equal('wo3 de mao1 xi3 huan1 he1 niu2 nai3')
 		})
 	})
 })
 
-describe('Hanzi (/h)', () => {
-	it('should get the right information about a Chinese character', () => {
-		return botCore.processMessage('/h 乐').then((data) => {
-			const lines = data.split('\n')
-			lines[0].should.equal('Character: 乐')
-			lines[1].should.equal('Pinyin: lè')
-			lines[2].should.equal('Cangjie: HVD : 竹女木')
-			lines[3].should.equal('Strokes: 5')
-			lines[4].should.equal('Definition: happy, glad; enjoyable; music')
-		})
-	})
-})
+// describe('Hanzi (/h)', () => {
+// 	it('should get the right information about a Chinese character', () => {
+// 		return botCore.processMessage('/h 乐').then(data => {
+// 			const lines = data.split('\n')
+// 			lines[0].should.equal('Character: 乐')
+// 			lines[1].should.equal('Pinyin: lè')
+// 			lines[2].should.equal('Cangjie: HVD : 竹女木')
+// 			lines[3].should.equal('Strokes: 5')
+// 			lines[4].should.equal('Definition: happy, glad; enjoyable; music')
+// 		})
+// 	})
+// })
