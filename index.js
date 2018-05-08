e6e81b5275cabd3f02317d3883e83ade8c12a21f
Pepe Becker <mail@pepebecker.com>
2018-05-08 02:11:36 +0100

    Add Pinyin to Zhuyin support

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mdbg = require('mdbg')
 const pinyinOrHanzi = require('pinyin-or-hanzi')
 const convert = require('pinyin-convert')
 const split = require('pinyin-split')
+const zhuyin = require('zhuyin')
 
 const getPinyin = async text => {
 	const type = await pinyinOrHanzi(text)
@@ -16,6 +17,27 @@ const getPinyin = async text => {
 				return part[0]
 			}
 		}).join('')
+	} else {
+		return text
+	}
+}
+
+const getZhuyin = async text => {
+	const type = await pinyinOrHanzi(text)
+	if (type !== 'other') {
+		const data = await convert(text, { segmented: true })
+		if (typeof data === 'string') {
+			return (await zhuyin.fromPinyin(data)).join(' ')
+		} else {
+			return Promise.all(data.map(async part => {
+				if (typeof part === 'string') {
+					return (await zhuyin.fromPinyin(part)).join(' ')
+				} else {
+					return (await zhuyin.fromPinyin(part[0])).join(' ')
+				}
+			}))
+			.then(list => list.join(' '))
+		}
 	} else {
 		return text
 	}
@@ -84,17 +106,20 @@ const getDescription = async text => {
 }
 
 const processMessage = async text => {
-	if (/^\/(s|split) /.test(text))
-	{
+	if (/^\/(s|split) /.test(text)) {
 		text = text.replace('/split ', '')
 		text = text.replace('/s ', '')
 		return getSplitted(text)
 	}
-	else if (/^\/(p|pinyin) /.test(text))
-	{
+	else if (/^\/(p|pinyin) /.test(text)) {
 		text = text.replace('/pinyin ', '')
 		text = text.replace('/p ', '')
 		return getPinyin(text)
+	}
+	else if (/^\/(z|zhuyin) /.test(text)) {
+		text = text.replace('/zhuyin ', '')
+		text = text.replace('/z ', '')
+		return getZhuyin(text)
 	}
 	else if (/^\/(d|definition) /.test(text)) {
 		text = text.replace('/definition ', '')
